c8e7e8fc9801ad164c7e11339c309d9f6b7313c2
mrsahadatarefin <sahadatarifin806@gmail.com>
2023-03-24 05:04:31 +0600

    something new add

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import img1 from '../assets/sahadat.png'
 import {MdOutlineKeyboardArrowRight} from 'react-icons/md'
+import { Link } from 'react-scroll';
 const Home = () => {
     return (
         <div name='home' className=' h-screen  bg-gradient-to-b from-black to-gray-800 via-black text-white '>
@@ -15,9 +16,9 @@ const Home = () => {
 
                 </p>
                 <div>
-                    <button className=' group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to bg-blue-500 cursor-pointer'>Projects
+                    <Link to='projects' className=' group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to bg-blue-500 cursor-pointer'>Projects
                         <span className='group-hover:rotate-90 duration-300'><MdOutlineKeyboardArrowRight size={30}className="ml-1" /></span>
-                    </button>
+                    </Link>
                 </div>
             </div>
             <div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -48,12 +48,12 @@ const NavBar = () => {
 
        {nav&&(
  <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
- {links.map((link) => (
+ {links.map(({link,id}) => (
     <li
     className="px-4 cursor-pointer capitalize py-6 text-4xl"
       key={link.id}
     >
-      {link.link}
+    <Link onClick={()=>setNav(!nav)} to={link}smooth duration={500}>{link}</Link>
     </li>
   ))}
   
