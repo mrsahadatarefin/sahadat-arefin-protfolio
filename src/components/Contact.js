7d1edfcd957205c22a29606bae7cdc7720f9b87c
mrsahadatarefin <sahadatarifin806@gmail.com>
2023-03-24 15:19:17 +0600

    new added

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,19 +12,19 @@ const Contact = () => {
         </div>
         <div className="flex justify-center items-center">
           <form action="https://getform.io/f/386073b3-6ebc-459a-acf0-9798d559a684" method="POST" className="flex flex-col w-full md:w-1/2">
-            <input
+            <input required
               className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
               type="text"
               name="name"
               placeholder="Enter your  name"
             />
-            <input
+            <input required
               className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none my-4"
               type="email"
               name="email"
               placeholder="Enter your email"
             />
-            <textarea
+            <textarea required
               placeholder="Enter your message"
               className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
               name="message"
